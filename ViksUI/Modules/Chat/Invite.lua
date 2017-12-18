cf4eacb3f50c1a0903c28452b4ab36abe7b09f95
Tryllemann <frode.h@aktivportal.com>
2017-12-18 18:33:24 +0100

    Alt click invite fix
    Fix for invite for players on another realm

--- ViksUI/Modules/Chat/Invite.lua
+++ ViksUI/Modules/Chat/Invite.lua
@@ -6,9 +6,16 @@ if C.chat.enable ~= true then return end
 ----------------------------------------------------------------------------------------
 hooksecurefunc("SetItemRef", function(link) --Secure hook to avoid taint
 	if IsAltKeyDown() then
-		local ChatFrameEditBox = ChatEdit_ChooseBoxForSend()
-		local player = link:match("^player:([^:]+)")
-		InviteToGroup(player)
-		ChatEdit_OnEscapePressed(ChatFrameEditBox) -- Secure hook opens whisper, so closing it.
+		local name = strsub(link, 8);
+		if (name and (strlen(name) > 0)) then
+			local begin, nend = string.find(name, "%s*[^%s:]+");
+			if (begin) then
+				name = strsub(name, begin, nend);
+			end
+			local ChatFrameEditBox = ChatEdit_ChooseBoxForSend()
+			--local player = link:match("^player:([^:]+)")
+			InviteToGroup(name)
+			ChatEdit_OnEscapePressed(ChatFrameEditBox) -- Secure hook opens whisper, so closing it.
+		end
 	end
 end)
