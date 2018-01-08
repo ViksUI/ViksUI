3a54a554589ebf9b137f5c6651862883353d989b
Tryllemann <frode.h@aktivportal.com>
2018-01-08 11:14:13 +0100

    Updated CombatText filter

--- ViksUI/Config/Filters/CombatText.lua
+++ ViksUI/Config/Filters/CombatText.lua
@@ -39,6 +39,8 @@ if C.combattext.merge_aoe_spam then
 	T.aoespam[207694] = 3			-- Symbiote Strike
 	T.aoespam[210380] = 4			-- Aura of Sacrifice
 	T.aoespam[252907] = 5			-- Torment the Weak
+	T.aoespam[252896] = 0.5			-- Chaotic Darkness
+	T.aoespam[253022] = 0.5			-- Sorrow
 end
 
 -- Class config
@@ -211,7 +213,8 @@ elseif T.class == "HUNTER" then
 elseif T.class == "MAGE" then
 	if C.combattext.merge_aoe_spam then
 		T.aoespam[217694] = 3.5		-- Living Bomb
-		T.aoespam[44461] = 3		-- Living Bomb (AoE)
+		T.aoespam[244813] = 3.5		-- Living Bomb
+		T.aoespam[44461] = 3.5		-- Living Bomb (AoE)
 		T.aoespam[2120] = 0			-- Flamestrike
 		T.aoespam[194432] = 0		-- Aftershocks
 		T.aoespam[12654] = 3		-- Ignite
