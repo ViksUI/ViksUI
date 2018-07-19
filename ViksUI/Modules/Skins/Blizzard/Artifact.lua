093daf8a53613b8101a901ae5562d59b0336b0d0
Tryllemann <frode.h@aktivportal.com>
2018-07-19 11:06:58 +0200

    Artifact Skin updated

--- ViksUI/Modules/Skins/Blizzard/Artifact.lua
+++ ViksUI/Modules/Skins/Blizzard/Artifact.lua
@@ -8,8 +8,6 @@ local function LoadSkin()
 	ArtifactFrame:StripTextures()
 	ArtifactFrame:SetTemplate("Transparent")
 	ArtifactFrame:CreateBackdrop()
-	T.SkinCloseButton(ArtifactFrame.CloseButton)
-
 	ArtifactFrame.BorderFrame:StripTextures()
 
 	for i = 1, 2 do
@@ -17,7 +15,18 @@ local function LoadSkin()
 	end
 	ArtifactFrameTab1:ClearAllPoints()
 	ArtifactFrameTab1:SetPoint("TOPLEFT", ArtifactFrame, "BOTTOMLEFT", 0, -2)
-	
+
+	ArtifactFrame.ForgeBadgeFrame.ItemIcon:Hide()
+	ArtifactFrame.ForgeBadgeFrame.ForgeLevelBackground:ClearAllPoints()
+	ArtifactFrame.ForgeBadgeFrame.ForgeLevelBackground:SetPoint("TOPLEFT", ArtifactFrame)
+
+	ArtifactFrame.KnowledgeLevelHelpBox:StripTextures()
+	ArtifactFrame.KnowledgeLevelHelpBox.Arrow:Hide()
+	ArtifactFrame.KnowledgeLevelHelpBox:CreateBackdrop("Transparent")
+
+	T.SkinCloseButton(ArtifactFrame.CloseButton)
+	T.SkinCloseButton(ArtifactFrame.KnowledgeLevelHelpBox.CloseButton)
+	T.SkinCloseButton(ArtifactFrame.AppearanceTabHelpBox.CloseButton)
 end
 
 T.SkinFuncs["Blizzard_ArtifactUI"] = LoadSkin
