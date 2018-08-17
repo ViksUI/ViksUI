e29a598d275e56a224a238f10b72e361ff608b52
Vik/Tryll <frode.h@aktivportal.com>
2018-08-17 20:12:36 +0200

    Revert "Filger improved"
    This reverts commit ba15f766609db582c7ca53bfb36cf49add76ac17.

--- ViksUI/Misc/Filger.lua
+++ ViksUI/Misc/Filger.lua
@@ -320,8 +320,7 @@ function Filger:DisplayActives()
 end
 
 function Filger:OnEvent(event, unit, _, spellID)
-	if event == "SPELL_UPDATE_COOLDOWN" or event == "PLAYER_TARGET_CHANGED" or event == "PLAYER_FOCUS_CHANGED" or event == "PLAYER_ENTERING_WORLD"
-	or (event == "UNIT_SPELLCAST_SUCCEEDED" and unit == "player") then
+	if event == "SPELL_UPDATE_COOLDOWN" or event == "PLAYER_TARGET_CHANGED" or event == "PLAYER_FOCUS_CHANGED" or event == "PLAYER_ENTERING_WORLD" or event == "UNIT_AURA" and (unit == "target" or unit == "player" or unit == "pet" or unit == "focus") or (event == "UNIT_SPELLCAST_SUCCEEDED" and unit == "player") then
 		local ptt = GetSpecialization()
 		local needUpdate = false
 		local id = self.Id
@@ -564,7 +563,7 @@ if C["filger_spells"] and C["filger_spells"][T.class] then
 					break
 				end
 			end
-			--frame:RegisterEvent("UNIT_AURA")
+			frame:RegisterEvent("UNIT_AURA")
 			frame:RegisterEvent("PLAYER_FOCUS_CHANGED")
 			frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 			frame:RegisterEvent("PLAYER_ENTERING_WORLD")
