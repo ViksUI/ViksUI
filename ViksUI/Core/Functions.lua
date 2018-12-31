21819180a88e17709480da46813e808d9e185f42
Frode <frode.h@aktivportal.com>
2018-12-31 12:50:42 +0100

    Fixed PetStablePrevPageButton/PetStableNextPageButton skin

--- ViksUI/Core/Functions.lua
+++ ViksUI/Core/Functions.lua
@@ -300,7 +300,7 @@ function T.SkinNextPrevButton(btn, left, scroll)
 
 	btn:StripTextures()
 
-	if scroll == "Up" or scroll == "Down" then
+	if scroll == "Up" or scroll == "Down" or scroll == "Any" then
 		normal = nil
 		pushed = nil
 		disabled = nil
