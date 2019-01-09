6698877cb424cee22350ec4c4e94c0c8f2c890bf
Frode <frode.h@aktivportal.com>
2019-01-09 18:13:19 +0100

    Fix Grid on /ui

--- ViksUI/Main/Move.lua
+++ ViksUI/Main/Move.lua
@@ -127,52 +127,30 @@ function Grid_Hide()
 	end
 end
 function Grid_Create() 
-	grid = CreateFrame('Frame', nil, UIParent) 
-	grid.boxSize = boxSize 
-	grid:SetAllPoints(UIParent) 
-
-	local size = 1 
-	local width = GetScreenWidth()
-	local ratio = width / GetScreenHeight()
-	local height = GetScreenHeight() * ratio
-
-	local wStep = width / boxSize
-	local hStep = height / boxSize
-
-	for i = 0, boxSize do 
-		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
-		if i == boxSize / 2 then 
-			tx:SetColorTexture(1, 0, 0, 0.5) 
-		else 
-			tx:SetColorTexture(0, 0, 0, 0.5) 
-		end 
-		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", i*wStep - (size/2), 0) 
-		tx:SetPoint('BOTTOMRIGHT', grid, 'BOTTOMLEFT', i*wStep + (size/2), 0) 
-	end 
-	height = GetScreenHeight()
-	
-	do
-		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
-		tx:SetColorTexture(1, 0, 0, 0.5)
-		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2) + (size/2))
-		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2 + size/2))
+	grid = CreateFrame("Frame", nil, UIParent)
+	grid:SetAllPoints(UIParent)
+	local width = GetScreenWidth() / 128
+	local height = GetScreenHeight() / 72
+	for i = 0, 128 do
+		local texture = grid:CreateTexture(nil, "BACKGROUND")
+		if i == 64 then
+			texture:SetColorTexture(1, 0, 0, 0.8)
+		else
+			texture:SetColorTexture(0, 0, 0, 0.8)
+		end
+		texture:SetPoint("TOPLEFT", grid, "TOPLEFT", i * width - 1, 0)
+		texture:SetPoint("BOTTOMRIGHT", grid, "BOTTOMLEFT", i * width, 0)
 	end
-	
-	for i = 1, math.floor((height/2)/hStep) do
-		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
-		tx:SetColorTexture(0, 0, 0, 0.5)
-		
-		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2+i*hStep) + (size/2))
-		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2+i*hStep + size/2))
-		
-		tx = grid:CreateTexture(nil, 'BACKGROUND') 
-		tx:SetColorTexture(0, 0, 0, 0.5)
-		
-		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2-i*hStep) + (size/2))
-		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2-i*hStep + size/2))
-		
+	for i = 0, 72 do
+		local texture = grid:CreateTexture(nil, "BACKGROUND")
+		if i == 36 then
+			texture:SetColorTexture(1, 0, 0, 0.8)
+		else
+			texture:SetColorTexture(0, 0, 0, 0.8)
+		end
+		texture:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -i * height)
+		texture:SetPoint("BOTTOMRIGHT", grid, "TOPRIGHT", 0, -i * height - 1)
 	end
-	
 end
 local function SlashCmd(cmd)
 	if InCombatLockdown() then print(ERR_NOT_IN_COMBAT) return end
