4d411c21d83566d4159d86c952bbd33ef2229f23
Frode <frode.h@aktivportal.com>
2019-06-28 00:04:08 +0200

    Added Punchards sockets

--- ViksUI/Modules/Skins/Blizzard/ItemSocketing.lua
+++ ViksUI/Modules/Skins/Blizzard/ItemSocketing.lua
@@ -16,6 +16,19 @@ local function LoadSkin()
 	ItemSocketingDescription:DisableDrawLayer("BORDER")
 	ItemSocketingDescription:DisableDrawLayer("BACKGROUND")
 
+	local gemTypeInfo ={
+		Yellow = {r=0.97, g=0.82, b=0.29},
+		Red = {r=1, g=0.47, b=0.47},
+		Blue = {r=0.47, g=0.67, b=1},
+		Hydraulic = {r=1, g=1, b=1},
+		Cogwheel = {r=1, g=1, b=1},
+		Meta = {r=1, g=1, b=1},
+		Prismatic = {r=1, g=1, b=1},
+		PunchcardRed = {r=1, g=0.47, b=0.47},
+		PunchcardYellow = {r=0.97, g=0.82, b=0.29},
+		PunchcardBlue = {r=0.47, g=0.67, b=1},
+	}
+
 	for i = 1, MAX_NUM_SOCKETS do
 		local button = _G["ItemSocketingSocket"..i]
 		local button_bracket = _G["ItemSocketingSocket"..i.."BracketFrame"]
@@ -39,7 +52,7 @@ local function LoadSkin()
 		for i = 1, numSockets do
 			local button = _G["ItemSocketingSocket"..i]
 			local gemColor = GetSocketTypes(i)
-			local color = GEM_TYPE_INFO[gemColor]
+			local color = gemTypeInfo[gemColor]
 			button:SetBackdropBorderColor(color.r, color.g, color.b)
 			button.overlay:SetVertexColor(color.r, color.g, color.b, 0.35)
 		end
