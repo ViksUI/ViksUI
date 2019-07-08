3b35a99878dd101af96d98d75c4f4ddb2e095164
Frode <frode.h@aktivportal.com>
2019-07-08 11:40:28 +0200

    Update Fixes.lua

--- ViksUI/Modules/Blizzard/Fixes.lua
+++ ViksUI/Modules/Blizzard/Fixes.lua
@@ -113,18 +113,3 @@ FrameStackFix:SetScript("OnEvent", function(self, event, addon)
 		end)
 	end
 end)
-
-----------------------------------------------------------------------------------------
---	Collect garbage
-----------------------------------------------------------------------------------------
-local eventcount = 0
-local Garbage = CreateFrame("Frame")
-Garbage:RegisterAllEvents()
-Garbage:SetScript("OnEvent", function(self, event)
-	eventcount = eventcount + 1
-
-	if (InCombatLockdown() and eventcount > 25000) or (not InCombatLockdown() and eventcount > 10000) or event == "PLAYER_ENTERING_WORLD" then
-		collectgarbage("collect")
-		eventcount = 0
-	end
-end)
