8e27ac82d6e01cd8e78f9bd5abe5255e65f9f0d1
Vik <frode.h@aktivportal.com>
2020-10-26 12:20:07 +0100

    Revert some changes

--- ViksUI/Core/Functions.lua
+++ ViksUI/Core/Functions.lua
@@ -501,6 +501,12 @@ function T.SkinCloseButton(f, point, text, pixel)
 		f.text:SetPoint("CENTER")
 		f.text:SetTexture(C.media.blank)
 	end
+	if text == "-" and pixel then
+		f.text = f:CreateTexture(nil, "OVERLAY")
+		f.text:SetSize(5, 1)
+		f.text:SetPoint("CENTER")
+		f.text:SetTexture(C.media.blank)
+	end
 	if not f.text then
 		if pixel then
 			f.text = f:FontString(nil, C.media.pixel_font, 8)
@@ -769,6 +775,46 @@ T.PostUpdateHealth = function(health, unit, min, max)
 		end
 	else
 		local r, g, b
+		if (C.unitframe.own_color ~= true and C.unitframe.enemy_health_color and unit == "target" and UnitIsEnemy(unit, "player") and UnitIsPlayer(unit)) or (C.unitframe.own_color ~= true and unit == "target" and not UnitIsPlayer(unit) and UnitIsFriend(unit, "player")) then
+			local c = T.Colors.reaction[UnitReaction(unit, "player")]
+			if c then
+				r, g, b = c[1], c[2], c[3]
+				health:SetStatusBarColor(r, g, b)
+			else
+				r, g, b = 0.3, 0.7, 0.3
+				health:SetStatusBarColor(r, g, b)
+			end
+		end
+		if unit == "pet" then
+			local _, class = UnitClass("player")
+			local r, g, b = unpack(T.Colors.class[class])
+			if C.unitframe.own_color == true then
+				health:SetStatusBarColor(unpack(C.unitframe.uf_color))
+				health.bg:SetVertexColor(0.1, 0.1, 0.1)
+			else
+				if b then
+					health:SetStatusBarColor(r, g, b)
+					if health.bg and health.bg.multiplier then
+						local mu = health.bg.multiplier
+						health.bg:SetVertexColor(r * mu, g * mu, b * mu)
+					end
+				end
+			end
+		end
+		if C.unitframe.bar_color_value == true and not UnitIsTapDenied(unit) then
+			if C.unitframe.own_color == true then
+				r, g, b = C.unitframe.uf_color[1], C.unitframe.uf_color[2], C.unitframe.uf_color[3]
+			else
+				r, g, b = health:GetStatusBarColor()
+			end
+			local newr, newg, newb = oUF:ColorGradient(min, max, 1, 0, 0, 1, 1, 0, r, g, b)
+
+			health:SetStatusBarColor(newr, newg, newb)
+			if health.bg and health.bg.multiplier then
+				local mu = health.bg.multiplier
+				health.bg:SetVertexColor(newr * mu, newg * mu, newb * mu)
+			end
+		end
 		if min ~= max then
 			r, g, b = oUF:ColorGradient(min, max, 0.69, 0.31, 0.31, 0.65, 0.63, 0.35, 0.33, 0.59, 0.33)
 			if (unit == "player" and not UnitHasVehicleUI("player") or unit == "vehicle") and health:GetAttribute("normalUnit") ~= "pet" then
@@ -830,54 +876,6 @@ T.PostUpdateHealth = function(health, unit, min, max)
 	end
 end
 
-T.PostUpdateHealthColor = function(health, unit, r, g, b)
-	if unit and unit:find("arena%dtarget") then return end
-	if not UnitIsConnected(unit) or UnitIsDead(unit) or UnitIsGhost(unit) then
-	else
-		local r, g, b
-		if (C.unitframe.own_color ~= true and C.unitframe.enemy_health_color and unit == "target" and UnitIsEnemy(unit, "player") and UnitIsPlayer(unit)) or (C.unitframe.own_color ~= true and unit == "target" and not UnitIsPlayer(unit) and UnitIsFriend(unit, "player")) then
-			local c = T.Colors.reaction[UnitReaction(unit, "player")]
-			if c then
-				r, g, b = c[1], c[2], c[3]
-				health:SetStatusBarColor(r, g, b)
-			else
-				r, g, b = 0.3, 0.7, 0.3
-				health:SetStatusBarColor(r, g, b)
-			end
-		end
-		if unit == "pet" then
-			local _, class = UnitClass("player")
-			local r, g, b = unpack(T.Colors.class.class[class])
-			if C.unitframe.own_color == true then
-				health:SetStatusBarColor(unpack(C.unitframe.uf_color))
-				health.bg:SetVertexColor(0.1, 0.1, 0.1)
-			else
-				if b then
-					health:SetStatusBarColor(r, g, b)
-					if health.bg and health.bg.multiplier then
-						local mu = health.bg.multiplier
-						health.bg:SetVertexColor(r * mu, g * mu, b * mu)
-					end
-				end
-			end
-		end
-		if C.unitframe.bar_color_value == true and not UnitIsTapDenied(unit) then
-			if C.unitframe.own_color == true then
-				r, g, b = C.unitframe.uf_color[1], C.unitframe.uf_color[2], C.unitframe.uf_color[3]
-			else
-				r, g, b = health:GetStatusBarColor()
-			end
-			local newr, newg, newb = oUF:ColorGradient(health.cur, health.max, 1, 0, 0, 1, 1, 0, r, g, b)
-
-			health:SetStatusBarColor(newr, newg, newb)
-			if health.bg and health.bg.multiplier then
-				local mu = health.bg.multiplier
-				health.bg:SetVertexColor(newr * mu, newg * mu, newb * mu)
-			end
-		end
-	end
-end
-
 T.PostUpdateRaidHealth = function(health, unit, min, max)
 	local self = health:GetParent()
 	local power = self.Power
@@ -893,6 +891,29 @@ T.PostUpdateRaidHealth = function(health, unit, min, max)
 		end
 	else
 		local r, g, b
+		if not UnitIsPlayer(unit) and UnitIsFriend(unit, "player") and C.unitframe.own_color ~= true then
+			local c = T.Colors.reaction[5]
+			local r, g, b = c[1], c[2], c[3]
+			health:SetStatusBarColor(r, g, b)
+			if health.bg and health.bg.multiplier then
+				local mu = health.bg.multiplier
+				health.bg:SetVertexColor(r * mu, g * mu, b * mu)
+			end
+		end
+		if C.unitframe.bar_color_value == true and not UnitIsTapDenied(unit) then
+			if C.unitframe.own_color == true then
+				r, g, b = C.unitframe.uf_color[1], C.unitframe.uf_color[2], C.unitframe.uf_color[3]
+			else
+				r, g, b = health:GetStatusBarColor()
+			end
+			local newr, newg, newb = oUF:ColorGradient(min, max, 1, 0, 0, 1, 1, 0, r, g, b)
+
+			health:SetStatusBarColor(newr, newg, newb)
+			if health.bg and health.bg.multiplier then
+				local mu = health.bg.multiplier
+				health.bg:SetVertexColor(newr * mu, newg * mu, newb * mu)
+			end
+		end
 		if min ~= max then
 			r, g, b = oUF:ColorGradient(min, max, 0.69, 0.31, 0.31, 0.65, 0.63, 0.35, 0.33, 0.59, 0.33)
 			if self:GetParent():GetName():match("oUF_PartyDPS") then
@@ -937,38 +958,7 @@ T.PostUpdateRaidHealth = function(health, unit, min, max)
 	end
 end
 
-T.PostUpdateRaidHealthColor = function(health, unit, r, g, b)
-	if not UnitIsConnected(unit) or UnitIsDead(unit) or UnitIsGhost(unit) then
-
-	else
-		local r, g, b
-		if not UnitIsPlayer(unit) and UnitIsFriend(unit, "player") and C.unitframe.own_color ~= true then
-			local c = T.Colors.reaction[5]
-			local r, g, b = c[1], c[2], c[3]
-			health:SetStatusBarColor(r, g, b)
-			if health.bg and health.bg.multiplier then
-				local mu = health.bg.multiplier
-				health.bg:SetVertexColor(r * mu, g * mu, b * mu)
-			end
-		end
-		if C.unitframe.bar_color_value == true and not UnitIsTapDenied(unit) then
-			if C.unitframe.own_color == true then
-				r, g, b = C.unitframe.uf_color[1], C.unitframe.uf_color[2], C.unitframe.uf_color[3]
-			else
-				r, g, b = health:GetStatusBarColor()
-			end
-			local newr, newg, newb = oUF:ColorGradient(health.cur, health.max, 1, 0, 0, 1, 1, 0, r, g, b)
-
-			health:SetStatusBarColor(newr, newg, newb)
-			if health.bg and health.bg.multiplier then
-				local mu = health.bg.multiplier
-				health.bg:SetVertexColor(newr * mu, newg * mu, newb * mu)
-			end
-		end
-	end
-end
-
-T.PostUpdatePowerColor = function(power, unit)
+T.PreUpdatePower = function(power, unit)
 	local _, pToken = UnitPowerType(unit)
 
 	local color = T.Colors.power[pToken]
@@ -1461,14 +1451,14 @@ T.CustomFilterBoss = function(_, unit, button, name, _, _, _, _, _, caster)
 	return true
 end
 
-T.UpdateThreat = function(self, _, unit)
-	if self.unit ~= unit then return end
-	local threat = UnitThreatSituation(self.unit)
-	if threat and threat > 1 then
-		local r, g, b = GetThreatStatusColor(threat)
-		self.backdrop:SetBackdropBorderColor(r, g, b)
+T.UpdateThreat = function(self, unit, status, r, g, b)
+	local parent = self:GetParent()
+	local badunit = not unit or parent.unit ~= unit
+
+	if not badunit and status and status > 1 then
+		parent.backdrop:SetBackdropBorderColor(r, g, b)
 	else
-		self.backdrop:SetBackdropBorderColor(unpack(C.media.border_color))
+		parent.backdrop:SetBackdropBorderColor(unpack(C.media.border_color))
 	end
 end
 
