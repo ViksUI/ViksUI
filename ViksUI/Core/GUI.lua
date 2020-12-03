b917e6b2c22e196fd0a487ee97f5eaee5fa0b8ea
Vik <frode.h@aktivportal.com>
2020-12-03 20:28:16 +0100

    Update GUI.lua

--- ViksUI/Core/GUI.lua
+++ ViksUI/Core/GUI.lua
@@ -1,8 +1,25 @@
 local T, C, L, _ = unpack(select(2, ...))
+
+-- Spells lists initialization
+C.filger.buff_spells_list = {}
+C.filger.proc_spells_list = {}
+C.filger.debuff_spells_list = {}
+C.filger.aura_bar_spells_list = {}
+C.filger.cd_spells_list = {}
+C.filger.ignore_spells_list = {}
+C.unitframe.plugins_aura_watch_list = {}
+C.announcements.spells_list = {}
+C.raidcooldown.spells_list = {}
+C.enemycooldown.spells_list = {}
+C.nameplate.debuffs_list = {}
+C.nameplate.buffs_list = {}
+C.media.profile = "-- Insert Your code here\n"
+C.options = {}
+
 if not IsAddOnLoaded("Viks_ConfigUI") then return end
 
 ----------------------------------------------------------------------------------------
---	This Module loads new user settings if ViksUI_Config is loaded
+--	This Module loads new user settings if Viks_ConfigUI is loaded
 ----------------------------------------------------------------------------------------
 -- Сreate the profile boolean
 if not ViksUIOptionsGlobal then ViksUIOptionsGlobal = {} end
@@ -22,21 +39,6 @@ if ViksUIOptionsGlobal[T.realm][T.name] == true then
 else
 	profile = ViksUIOptions
 end
-
--- Spells list
-C["filger"].buff_spells_list = {}
-C["filger"].proc_spells_list = {}
-C["filger"].debuff_spells_list = {}
-C["filger"].aura_bar_spells_list = {}
-C["filger"].cd_spells_list = {}
-C["filger"].ignore_spells_list = {}
-C["unitframe"].plugins_aura_watch_list = {}
-C["announcements"].spells_list = {}
-C["raidcooldown"].spells_list = {}
-C["enemycooldown"].spells_list = {}
-C["nameplate"].debuffs_list = {}
-C["nameplate"].buffs_list = {}
-C["media"].profile = "-- Insert Your code here\n"
 
 -- Apply or remove saved settings as needed
 for group, options in pairs(profile) do
