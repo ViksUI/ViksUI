5314128b23fcb23462730ddb2adcc90f54a6ff97
Frode <frode.h@aktivportal.com>
2022-11-04 20:13:47 +0100

    CodeChange

--- ViksUI/Core/WTF.lua
+++ ViksUI/Core/WTF.lua
@@ -1,1 +1,1 @@
-﻿local T, C, L, _ = unpack(select(2, ...))
+local T, C, L, _ = unpack(select(2, ...))
--- ViksUI/Locales/English.lua
+++ ViksUI/Locales/English.lua
@@ -1,4 +1,4 @@
-﻿local T, C, L, _ = unpack(select(2, ...))
+local T, C, L, _ = unpack(select(2, ...))
 
 ----------------------------------------------------------------------------------------
 --	Localization for enUS and enGB client
