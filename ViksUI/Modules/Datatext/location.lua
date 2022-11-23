9f2d0c93a16fb88d1a6aa941bf621e0f53ca811e
Frode <frode.h@aktivportal.com>
2022-11-23 01:04:56 +0100

    [Location] Improved memory leak from Cords x/y

--- ViksUI/Modules/Datatext/location.lua
+++ ViksUI/Modules/Datatext/location.lua
@@ -2,33 +2,21 @@ local T, C, L, _ = unpack(select(2, ...))
 --------------------------------------------------------------------
 -- Location and Coords
 --------------------------------------------------------------------
-local Unknown = UNKNOWN
-local cordson = C.datatext.showcoords
+if not C.datatext.location and not C.datatext.location > 0 then return end
 
-local ZoneColors = {
-	["friendly"] = {0.1, 1.0, 0.1},
-	["sanctuary"] = {0.41, 0.8, 0.94},
-	["arena"] = {1.0, 0.1, 0.1},
-	["hostile"] = {1.0, 0.1, 0.1},
-	["contested"] = {1.0, 0.7, 0},
-	["combat"] = {1.0, 0.1, 0.1},
-	["else"] = {1.0, 0.9294, 0.7607}
-}
+local Stat = CreateFrame("Frame", "DataTextLocation", UIParent)
+Stat:SetFrameStrata("BACKGROUND")
+Stat:SetFrameLevel(3)
 
-if C.datatext.location and C.datatext.location > 0 then
-	local Stat = CreateFrame("Frame", "DataTextLocation", UIParent)
-	Stat:SetFrameStrata("BACKGROUND")
-	Stat:SetFrameLevel(3)
-
-	local Text  = Stat:CreateFontString(nil, "OVERLAY")
+local Text  = Stat:CreateFontString(nil, "OVERLAY")
 if C.datatext.location >= 9 then
-Text:SetTextColor(unpack(C.media.pxcolor1))
-Text:SetFont(C.media.pxfontHeader, C.media.pxfontHsize, C.media.pxfontHFlag)
+	Text:SetTextColor(unpack(C.media.pxcolor1))
+	Text:SetFont(C.media.pxfontHeader, C.media.pxfontHsize, C.media.pxfontHFlag)
 else
-Text:SetTextColor(unpack(C.media.pxcolor1))
-Text:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
+	Text:SetTextColor(unpack(C.media.pxcolor1))
+	Text:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
 end
-	PP(C.datatext.location, Text)
+PP(C.datatext.location, Text)	
 
 function LocMopColoring()
 	local pvpType = GetZonePVPInfo()
@@ -49,40 +37,49 @@ function LocMopColoring()
 	end
 end
 
-local ela = 0
-local function Update(self, t)
-	local curTime = GetTime()
-	if curTime - ela <= 0.2 then return end
-	ela = curTime
+local mapRects, tempVec2D = {}, CreateVector2D(0, 0)
+local function GetPlayerMapPos(mapID)
+	tempVec2D.x, tempVec2D.y = UnitPosition("player")
+	if not tempVec2D.x then return end
 
+	local mapRect = mapRects[mapID]
+	if not mapRect then
+		local _, pos1 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0))
+		local _, pos2 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1))
+		if not pos1 or not pos2 then return end
+		mapRect = {pos1, pos2}
+		mapRect[2]:Subtract(mapRect[1])
+		mapRects[mapID] = mapRect
+	end
+	tempVec2D:Subtract(mapRect[1])
+
+	return (tempVec2D.y/mapRect[2].y), (tempVec2D.x/mapRect[2].x)
+end
+
+local int = 0
+local function Update(self, t)
 	local subZoneText = GetMinimapZoneText() or ""
 	local zoneText = _G.GetRealZoneText() or _G.UNKNOWN;
-	
-	if cordson == true then
-		local unitMap = C_Map.GetBestMapForUnit("player")
-		local mapRects, tempVec2D = {}, CreateVector2D(0, 0)
-		local function GetPlayerMapPos(mapID)
-			tempVec2D.x, tempVec2D.y = UnitPosition("player")
-			if not tempVec2D.x then return end
 
-			local mapRect = mapRects[mapID]
-			if not mapRect then
-				local _, pos1 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0))
-				local _, pos2 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1))
-				if not pos1 or not pos2 then return end
-
-				mapRect = { pos1, pos2}
-				mapRect[2]:Subtract(mapRect[1])
-				mapRects[mapID] = mapRect
-			end
-			tempVec2D:Subtract(mapRect[1])
-
-			return (tempVec2D.y/mapRect[2].y), (tempVec2D.x/mapRect[2].x)
+	if C.datatext.location and C.datatext.location > 0 then
+		if (subZoneText ~= "") and (subZoneText ~= zoneText) then
+			displayLine = zoneText .. ": " .. subZoneText
+		else
+			displayLine = subZoneText
 		end
+	else
+		displayLine = subZoneText
+	end
+		
+	local r, g, b = LocMopColoring()
+	int = int + 1
+	if int >= 3 then
+		local unitMap = C_Map.GetBestMapForUnit("player")
+		--local x, y = 0, 0
+
 		if unitMap then
 			x, y = GetPlayerMapPos(unitMap)
 		end
-		
 		if (not x) and (not y) then
 			x = 0
 			y = 0
@@ -91,30 +88,28 @@ local function Update(self, t)
 		x = math.floor(100 * x) or 0
 		y = math.floor(100 * y) or 0
 		
-		local eventcount = 0
-		if (InCombatLockdown() and eventcount > 5000) or (not InCombatLockdown() and eventcount > 5000) or event == "PLAYER_ENTERING_WORLD" then
-			collectgarbage("collect")
-			eventcount = 0
-		end
-	end
-	local displayLine
-	-- zone and subzone
-	if C.datatext.location and C.datatext.location > 0 then
-		if (subZoneText ~= "") and (subZoneText ~= zoneText) then
-			displayLine = zoneText .. ": " .. subZoneText
-			else
-			displayLine = subZoneText
-			end
-		else
-			displayLine = subZoneText
-		end
-		local r, g, b = LocMopColoring()
-		if cordson == true then
+		if C.datatext.showcoords == true then
 			Text:SetText(qColor..x.." ".. string.format("|cff%02x%02x%02x%s|r", r*255, g*255, b*255, displayLine)..qColor.." "..y)
 		else
 			Text:SetText(string.format("|cff%02x%02x%02x%s|r", r*255, g*255, b*255, displayLine))
 		end
+		int = 0
 	end
-	Stat:SetScript("OnUpdate", Update)
-	Update(Stat, 10)
 end
+
+Stat:SetScript("OnUpdate", Update)
+Update(Stat, 10)
+
+-- Collect memory garbage
+local eventcount = 0
+local VikInGame = CreateFrame("Frame")
+VikInGame:RegisterAllEvents()
+VikInGame:SetScript("OnEvent", function(self, event)
+	eventcount = eventcount + 1
+	if InCombatLockdown() then return end
+
+	if eventcount > 6000 then
+		collectgarbage("collect")
+		eventcount = 0
+	end
+end)
