96dbb249331592e192b48e99a8400ff1c6c03cb5
Frode <frode.h@aktivportal.com>
2024-08-06 02:30:42 +0200

    [ChatFrames] - Option to show Role Icons in Chat

--- ViksUI/Modules/Chat/ChatFrames.lua
+++ ViksUI/Modules/Chat/ChatFrames.lua
@@ -185,7 +185,6 @@ _G.CHAT_FLAG_GM = "|cff4154F5"..L_CHAT_GM.."|r "
 _G.ERR_FRIEND_ONLINE_SS = "|Hplayer:%s|h[%s]|h "..L_CHAT_COME_ONLINE
 _G.ERR_FRIEND_OFFLINE_S = "[%s] "..L_CHAT_GONE_OFFLINE
 
-
 -- Hide chat bubble menu button
 ChatFrameMenuButton:Kill()
 
@@ -578,6 +577,41 @@ local function UpdateTabChannelSwitch(self)
 	end
 end
 hooksecurefunc("ChatEdit_CustomTabPressed", UpdateTabChannelSwitch)
+
+----------------------------------------------------------------------------------------
+--	Role icons
+----------------------------------------------------------------------------------------
+if C.chat.role_icons == true then
+	local chats = {
+		CHAT_MSG_SAY = 1, CHAT_MSG_YELL = 1,
+		CHAT_MSG_WHISPER = 1, CHAT_MSG_WHISPER_INFORM = 1,
+		CHAT_MSG_PARTY = 1, CHAT_MSG_PARTY_LEADER = 1,
+		CHAT_MSG_INSTANCE_CHAT = 1, CHAT_MSG_INSTANCE_CHAT_LEADER = 1,
+		CHAT_MSG_RAID = 1, CHAT_MSG_RAID_LEADER = 1, CHAT_MSG_RAID_WARNING = 1,
+	}
+
+	local role_tex = {
+		TANK = "\124T"..[[Interface\AddOns\ViksUI\Media\Textures\Tank.tga]]..":12:12:0:0:64:64:5:59:5:59\124t",
+		HEALER	= "\124T"..[[Interface\AddOns\ViksUI\Media\Textures\Healer.tga]]..":12:12:0:0:64:64:5:59:5:59\124t",
+		DAMAGER = "\124T"..[[Interface\AddOns\ViksUI\Media\Textures\Damager.tga]]..":12:12:0:0:64:64:5:59:5:59\124t",
+	}
+
+	local GetColoredName_orig = _G.GetColoredName
+	local function GetColoredName_hook(event, arg1, arg2, ...)
+		local ret = GetColoredName_orig(event, arg1, arg2, ...)
+		if chats[event] then
+			local role = UnitGroupRolesAssigned(arg2)
+			if role == "NONE" and arg2:match(" *- *"..GetRealmName().."$") then
+				role = UnitGroupRolesAssigned(arg2:gsub(" *-[^-]+$",""))
+			end
+			if role and role ~= "NONE" then
+				ret = role_tex[role]..""..ret
+			end
+		end
+		return ret
+	end
+	_G.GetColoredName = GetColoredName_hook
+end
 
 ----------------------------------------------------------------------------------------
 --	Prevent reposition ChatFrame
