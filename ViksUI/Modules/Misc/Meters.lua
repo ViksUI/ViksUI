00b669bcdfcbad3fff03254ee15989c61d095208
Frode <frode.h@aktivportal.com>
2024-08-07 13:14:28 +0200

    [Meters] New chat window layout,
    Since right chat is Trade/Loot, it will only hide/show the chat window.

--- ViksUI/Modules/Misc/Meters.lua
+++ ViksUI/Modules/Misc/Meters.lua
@@ -6,7 +6,7 @@ local CreateBtn = T.CreateBtn
 local thickness = 1
 
 -----------------------------------------------------------------------------------------
--- METERS 
+-- METERS
 -----------------------------------------------------------------------------------------
 
 if IsAddOnLoaded('Skada') or IsAddOnLoaded('Details') or IsAddOnLoaded('Recount') then
@@ -15,14 +15,14 @@ if IsAddOnLoaded('Skada') or IsAddOnLoaded('Details') or IsAddOnLoaded('Recount'
 	RIGHTChatline:SetFrameLevel(15)
 	ShowHideMeters:SetPoint("LEFT", RIGHTChatline, "LEFT", 0, 0)
 	if IsAddOnLoaded('Skada') then
-		ShowHideMeters:SetAttribute("macrotext1", "/chatU\n/Skada toggle")
+		ShowHideMeters:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(0)\n/run ChatFrame4Tab:Hide()\n/Skada toggle")
 	end
 	if IsAddOnLoaded('Recount') then
-		ShowHideMeters:SetAttribute("macrotext1", "/chatU\n/recount toggle")
-	end	
+		ShowHideMeters:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(0)\n/run ChatFrame4Tab:Hide()\n/recount toggle")
+	end
 	if IsAddOnLoaded('Details') then
-		ShowHideMeters:SetAttribute("macrotext1", "/chatU\n/details toggle")
-	end	
+		ShowHideMeters:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(0)\n/run ChatFrame4Tab:Hide()\n/details toggle")
+	end
 	ShowHideMeters:SetAlpha(0)
 
 	local DamageMeterText = RIGHTChatline:CreateFontString(nil, "OVERLAY")
@@ -43,7 +43,6 @@ if IsAddOnLoaded('Skada') or IsAddOnLoaded('Details') or IsAddOnLoaded('Recount'
 		RIGHTChatline.iconleft:SetPoint("LEFT", RIGHTChatline, "LEFT", C.panels.CPwidth/6, 0)
 	end
 
-		
 	ShowHideMeters:SetScript("OnEnter", function(self)
 		DamageMeterText:SetTextColor(class.r, class.g, class.b)
 		GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, 16)
@@ -52,33 +51,32 @@ if IsAddOnLoaded('Skada') or IsAddOnLoaded('Details') or IsAddOnLoaded('Recount'
 		GameTooltip:AddLine("(Skada, Recount, Details) ", 1, 1, 1, 1, 1, 1)
 		GameTooltip:Show()
 	end)
-	
+
 	ShowHideMeters:SetScript("OnLeave", function(self)
 		DamageMeterText:SetTextColor(unpack(C.media.pxcolor1))
 		GameTooltip:Hide()
 	end)
-		
-	-- Close Button // Not perfect solution
+
 	CreateBtn("resetChat", RIGHTChatline, 70, 20, "", "")
 	resetChat:SetPoint("LEFT", RIGHTChatline, "LEFT", 60, 0)
 	if IsAddOnLoaded('Skada') then
-		resetChat:SetAttribute("macrotext1", "/chatS\n/Skada toggle")
+		resetChat:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(1)\n/run ChatFrame4Tab:Show()\n/Skada toggle")
 	end
 	if IsAddOnLoaded('Recount') then
-		resetChat:SetAttribute("macrotext1", "/chatS\n/recound toggle")
-	end	
+		resetChat:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(1)\n/run ChatFrame4Tab:Show()\n/recound toggle")
+	end
 	if IsAddOnLoaded('Details') then
-		resetChat:SetAttribute("macrotext1", "/chatS\n/details hide")
-	end	
+		resetChat:SetAttribute("macrotext1", "/run ChatFrame4:SetAlpha(1)\n/run ChatFrame4Tab:Show()\n/details hide")
+	end
 
 	resetChat:SetAlpha(0)
-	
+
 	local resetChatText = RIGHTChatline:CreateFontString(nil, "OVERLAY")
 	resetChatText:SetFont(C.media.pixel_font, C.media.pixel_font_size-2, C.media.pixel_font_style)
 	resetChatText:SetPoint("LEFT", RIGHTChatline, "LEFT", 65, -2)
 	resetChatText:SetTextColor(unpack(C.media.pxcolor1))
 	resetChatText:SetText(" CHAT")
-	
+
 	resetChat:SetScript("OnEnter", function(self)
 		resetChatText:SetTextColor(class.r, class.g, class.b)
 		GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, 16)
@@ -86,10 +84,9 @@ if IsAddOnLoaded('Skada') or IsAddOnLoaded('Details') or IsAddOnLoaded('Recount'
 		GameTooltip:AddLine("Reset Chat and hide Meters ", 1, 1, 1, 1, 1, 1)
 		GameTooltip:Show()
 	end)
-	
+
 	resetChat:SetScript("OnLeave", function(self)
 		resetChatText:SetTextColor(unpack(C.media.pxcolor1))
 		GameTooltip:Hide()
 	end)
-	
 end
