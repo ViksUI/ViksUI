0ca7011c07efe00264692e661ee86ac70fb4323e
Frode <frode.h@aktivportal.com>
2024-08-07 13:12:38 +0200

    [Minimap Buttons] Removed old raid function

--- ViksUI/Modules/Maps/MiniMapButtons.lua
+++ ViksUI/Modules/Maps/MiniMapButtons.lua
@@ -2,62 +2,6 @@ local T, C, L = unpack(ViksUI)
 if C.minimap.enable ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	Switch layout mouseover button on minimap
-----------------------------------------------------------------------------------------
-local switch = CreateFrame("Button", "SwitchLayout", UIParent)
-switch:SetTemplate("Transparent")
-
-switch:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMLEFT", -3, 39)
-switch:SetSize(19, 19)
-switch:SetAlpha(0)
-
-switch.t = switch:CreateTexture(nil, "OVERLAY")
-switch.t:SetTexture("Interface\\LFGFrame\\LFGROLE")
-switch.t:SetPoint("TOPLEFT", switch, 2, -2)
-switch.t:SetPoint("BOTTOMRIGHT", switch, -2, 2)
-
-switch:EnableMouse(true)
-switch:RegisterForClicks("AnyUp")
-switch:SetScript("OnClick", function(_, button)
-	if button == "LeftButton" and ViksUISettingsPerChar.RaidLayout ~= "HEAL" then
-		ViksUIOptionsPerChar.unitframe.HealFrames = true
-		ViksUIOptions.unitframe.HealFrames = true
-		ViksUISettingsPerChar.RaidLayout = "HEAL"
-		ReloadUI()
-	elseif button == "RightButton" and ViksUISettingsPerChar.RaidLayout ~= "DPS" then
-		ViksUIOptionsPerChar.unitframe.HealFrames = false
-		ViksUIOptions.unitframe.HealFrames = false
-		ViksUISettingsPerChar.RaidLayout = "DPS"
-		ReloadUI()
-	end
-end)
-
-switch:SetScript("OnEnter", function()
-	switch:FadeIn()
-	GameTooltip:SetOwner(switch, "ANCHOR_LEFT")
-	GameTooltip:AddLine(RAID_FRAMES_LABEL)
-	GameTooltip:AddLine(" ")
-	GameTooltip:AddLine(L_MINIMAP_HEAL_LAYOUT)
-	GameTooltip:AddLine(L_MINIMAP_DPS_LAYOUT)
-	--GameTooltip:AddLine(L_MINIMAP_BLIZZ_LAYOUT)
-	GameTooltip:Show()
-end)
-
-switch:SetScript("OnLeave", function()
-	switch:FadeOut()
-	GameTooltip:Hide()
-end)
-
-switch:RegisterEvent("PLAYER_LOGIN")
-switch:SetScript("OnEvent", function()
-	if ViksUISettings and (ViksUIOptionsPerChar.unitframe.HealFrames ~= true or ViksUIOptions.unitframe.HealFrames ~= true) then
-		switch.t:SetTexCoord(0.25, 0.5, 0, 1)
-	elseif ViksUISettings and (ViksUIOptionsPerChar.unitframe.HealFrames == true or ViksUIOptions.unitframe.HealFrames == true) then
-		switch.t:SetTexCoord(0.75, 1, 0, 1)
-	end
-end)
-
-----------------------------------------------------------------------------------------
 --	Farm mode for minimap(by Elv22)
 ----------------------------------------------------------------------------------------
 T.FarmMode = false
@@ -105,7 +49,7 @@ end)
 farm:SetScript("OnEnter", function()
 
 	farm:FadeIn()
-	GameTooltip:SetOwner(switch, "ANCHOR_LEFT")
+	GameTooltip:SetOwner(farm, "ANCHOR_LEFT")
 	GameTooltip:AddLine(L_MINIMAP_FARM, 0.40, 0.78, 1)
 	GameTooltip:Show()
 end)
