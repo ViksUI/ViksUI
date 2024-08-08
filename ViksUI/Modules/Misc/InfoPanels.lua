1cfd4e97293fb7134664835bf93268efcc4fd52a
Frode <frode.h@aktivportal.com>
2024-08-08 15:28:03 +0200

    [InfoPanels]Reworked Stats panel above character frame
    Stats from Wowhead

--- ViksUI/Modules/Misc/InfoPanels.lua
+++ ViksUI/Modules/Misc/InfoPanels.lua
@@ -1,84 +1,125 @@
 local T, C, L = unpack(ViksUI)
+
 if C.misc.InfoPanel_Stats ~= true then return end
 ----------------------------------------------------------------------------------------
--- Stats Panel with info from https://www.icy-veins.com - Credit to Wyr3d and Papasol for the idea
+-- Stats Panel with info currently from Wowhead
 ----------------------------------------------------------------------------------------
-local IcyVeinStats = {}
-IcyVeinStats["DEATHKNIGHT-250"] = "iLvL > Haste > Crit = Mast > Vers" --// Blood DK
-IcyVeinStats["DEATHKNIGHT-251"] = "Crit > Mast > Haste > Vers" --// Frost DK
-IcyVeinStats["DEATHKNIGHT-252"] = "|cFFFFFF00General:|r Haste > Mast > Crit > Vers |cFFFFFF00AoE:|r Mast > Crit > Haste > Vers" --// Unholy DK
+local InfoStats = {}
+--DH Havoc
+InfoStats["DEMONHUNTER-577"] = "Single Target: Agility > Critical Strike > Mastery > Haste > Versatility \n Multiple Target: Agility > Critical Strike > Mastery > Versatility > Haste"
+--DH Vengeance
+InfoStats["DEMONHUNTER-581"] = "Agility > Critical Strike > Haste > Versatility > Mastery"
 
-IcyVeinStats["DRUID-102"] = "Int > Mast > Haste > Crit > Vers" --// Boomkin Druid
-IcyVeinStats["DRUID-103"] = "Agil > Crit > Mast > Haste > Vers" --// Feral Druid
-IcyVeinStats["DRUID-104"] = "|cFFFFFF00Surv:|r iLvL > Armor = Agil = Stam > Haste > Vers > Mast |cFFFFFF00Dmg:|r Agil > Vers = Haste > Crit = Mast" --// Bear Druid
-IcyVeinStats["DRUID-105"] = "|cFFFFFF00Raid:|r Int > Haste > Mast > Crit = Vers|cFFFFFF00Dung+dmg:|r Int > Haste > Vers > Crit > Mast" --// Restro Druid
+--DEATHKNIGHT Blood
+InfoStats["DEATHKNIGHT-250"] = "Item Level > Haste = Critical Strike = Versatility = Mastery"
+--DEATHKNIGHT Frost
+InfoStats["DEATHKNIGHT-251"] = "Critical Strike > Mastery > Haste > Versatility"
+--DEATHKNIGHT Unholy
+InfoStats["DEATHKNIGHT-252"] = "Single Target: Mastery > Haste > Critical Strike > Versatility \n Multiple Target: Mastery > Critical Strike > Haste > Versatility"
 
-IcyVeinStats["EVOKER-1467"] = "Int/iLvL > Mast > Haste > Crit > Vers" --// Devastation
-IcyVeinStats["EVOKER-1468"] = "|cFFFFFF00Raid:|r Int > Mast > Crit  > Vers > Haste |cFFFFFF00Dung:|r Int > Crit > Haste > Vers > Mast" --// Preservation
+--DRUID Balance
+InfoStats["DRUID-102"] = "Intellect > Mastery > Haste > Critical Strike > Versatility"
+--DRUID Feral
+InfoStats["DRUID-103"] = "Single Target: Agility > Mastery > Critical Strike > Haste > Versatility \n Multiple Target: Agility > Mastery > Critical Strike > Haste = Versatility"
+--DRUID Guardian
+InfoStats["DRUID-104"] = "Surv: Agility > Haste > Versatility > Mastery > Critical Strike \n DPS: Agility > Versatility = Haste > Critical Strike = Mastery"
+--DRUID Restoration
+InfoStats["DRUID-105"] = "Raid: Intellect > Haste > Mastery > Critical Strike = Versatility \n Dung: Intellect > Mastery = Haste > Versatility > Critical Strike \n Dung Dps: Intellect > Haste > Versatility > Critical Strike > Mastery"
 
-IcyVeinStats["HUNTER-253"] = "Haste > Crit > Mast > Vers" --// Beast Master Hunter
-IcyVeinStats["HUNTER-254"] = "Mast > Crit > Vers > Haste" --// Marksman Hunter
-IcyVeinStats["HUNTER-255"] = "Haste > Crit > Vers > Mast" --// Survival Hunter
+--EVOKER Devastation
+InfoStats["EVOKER-1467"] = "Intellect > Critical Strike >= Versatility = Mastery = Haste"
+--EVOKER Preservation
+InfoStats["EVOKER-1468"] = "Myth+: Intellect > Critical Strike > Haste > Versatility > Mastery \n Raid: Intellect > Mastery > Critical Strike > Versatility > Haste"
+--EVOKER Augmentation
+InfoStats["EVOKER-1473"] = "Intellect > Mastery > Critical Strike > Haste > Versatility"
 
-IcyVeinStats["MAGE-62"] = "Int > Crit > Mast > Vers > Haste" --// Arcane Mage
-IcyVeinStats["MAGE-63"] = "Int > Haste > Vers > Mast > Crit" --// Fire Mage
-IcyVeinStats["MAGE-64"] = "Int > Mast > Haste > Crit (to 33.34%) > Vers" --// Frost Mage
+--HUNTER BM
+InfoStats["HUNTER-253"] = "Haste 30% > Critical Strike 44% > Mastery 540 > Versatility"
+--HUNTER MM
+InfoStats["HUNTER-254"] = "Haste 30% > Critical Strike 44% > Mastery = Versatility"
+--HUNTER SV
+InfoStats["HUNTER-255"] = "Haste 30% > Critical Strike 44% > Versatility > Haste = Critical Strike > Mastery"
 
-IcyVeinStats["MONK-268"] = "|cFFFFFF00Defensive:|r iLvL > Vers = Mast = Crit > Haste |cFFFFFF00Offensive:|r Vers = Crit > Haste > Mast" --// Brewmaster Monk
-IcyVeinStats["MONK-269"] = "Weapon Dmg > Agil > Vers = Crit > Mast > Haste" --// Windwalker Monk
-IcyVeinStats["MONK-270"] = "|cFFFFFF00Raid:|r Int > Haste > Crit = Mast > Vers |cFFFFFF00Dung:|r Int > Haste > Mast > Vers > Crit" --// Mistweaver Monk
+--MAGE Arcane
+InfoStats["MAGE-62"] = "Intellect > Critical Strike > Mastery > Haste > Versatility"
+--MAGE Fire
+InfoStats["MAGE-63"] = "Intellect > Haste > Critical Strike > Versatility > Mastery"
+--MAGE Frost
+InfoStats["MAGE-64"] = "Intellect > Mastery > Haste > Critical Strike 33.34% > Versatility"
 
-IcyVeinStats["PALADIN-65"] = "|cFFFFFF00Raid:|r Int > Haste > Mast > Vers > Crit |cFFFFFF00Dung:|r Int > Haste > Vers > Crit > Mast" --// Holy Paladin
-IcyVeinStats["PALADIN-66"] = "Haste ≥ Mast ≥ Vers > Crit" --// Protection Paladin
-IcyVeinStats["PALADIN-70"] = "Str > Mast > Haste ~ Crit = Vers" --// Retribution Paladin
+--MONK Brewmaster
+InfoStats["MONK-268"] = "Defensive: Agility > Versatility = Mastery = Critical Strike > Haste \n Offensive: Agility > Versatility = Critical Strike > Mastery > Haste"
+--MONK Windwalker
+InfoStats["MONK-269"] = "Weapon Damage > Agility > Versatility = Critical Strike > Mastery > Haste"
+--MONK Mistweaver
+InfoStats["MONK-270"] = "Myth+: Intellect > Haste > Mastery > Versatility > Critical Strike \n Raid: Intellect > Haste > Critical Strike > Versatility > Mastery"
 
-IcyVeinStats["PRIEST-256"] = "Int > Haste > Crit > Vers > Mast" --// Disc Priest
-IcyVeinStats["PRIEST-257"] = "|cFFFFFF00Raid:|r Int > Mast = Crit > Vers > Haste |cFFFFFF00Dung:|r Int > Crit = Haste > Vers > Mast" --// Holy Priest
-IcyVeinStats["PRIEST-258"] = "Int > Haste > Mast > Crit = Vers" --// Shadow Priest
+--PALA Holy
+InfoStats["PALADIN-65"] = "Raid: Intellect > Mastery = Critical Strike > Versatility > Haste \n Myth+: Intellect > Critical Strike = Haste > Versatility > Mastery"
+--PALA Protection
+InfoStats["PALADIN-66"] = "Strength > Haste ≥ Mastery ≥ Versatility > Critical Strike"
+--PALA Retribution
+InfoStats["PALADIN-70"] = "Strength > Haste > Versatility = Critical Strike > Mastery"
 
-IcyVeinStats["ROGUE-259"] = "Crit > Mast > Haste > Vers" --// Assassin Rogue
-IcyVeinStats["ROGUE-260"] = "Vers > Crit > Mast > Haste" --// Outlaw Rogue
-IcyVeinStats["ROGUE-261"] = "|cFFFFFF00ST:|r Crit > Vers > Haste > Mast |cFFFFFF00AoE:|r Crit > Vers > Mast > Haste" --// Subtley Rogue
+--PRIESTDiscipline
+InfoStats["PRIEST-256"] = "Intellect > Haste > Critical Strike > Versatility > Mastery"
+--PRIESTHoly
+InfoStats["PRIEST-257"] = "Myth+: Intellect > Critical Strike = Haste > Versatility > Mastery \n Raid: Intellect > Mastery = Critical Strike > Versatility > Haste"
+--PRIESTShadow
+InfoStats["PRIEST-258"] = "Intellect > Haste > Mastery > Critical Strike > Versatility"
 
-IcyVeinStats["SHAMAN-262"] = "|cFFFFFF00ST:|r Int > Mast > Haste > Vers > Crit |cFFFFFF00AoE:|r Int > Haste > Crit > Vers > Mast" --// Elemental Shaman
-IcyVeinStats["SHAMAN-263"] = "Agil > Haste = Mast > Vers > Crit" --// Enhancement Shaman
-IcyVeinStats["SHAMAN-264"] = "Int > Vers = Crit > Haste = Mast"  --// Restro Shaman
+--ROGUE Assassination
+InfoStats["ROGUE-259"] = "Critical Strike > Mastery > Versatility > Haste"
+--ROGUE Outlaw
+InfoStats["ROGUE-260"] = "Versatility > Critical Strike > Mastery > Haste"
+--ROGUE Subtlety
+InfoStats["ROGUE-261"] = "Critical Strike > Mastery > Versatility > Haste"
 
-IcyVeinStats["WARLOCK-265"] = "Int > Mast > Haste > Crit > Vers" --// Affliction Warlock
-IcyVeinStats["WARLOCK-266"] = "|cFFFFFF00ST:|r Int > Haste > Crit > Mast ~ Vers |cFFFFFF00AoE:|r Int > Haste > Crit ~ Mast > Vers" --// Demonology Warlock
-IcyVeinStats["WARLOCK-267"] = "Int > Haste ≥ Mast > Crit > Vers" --// Destruction Warlock
+--SHAMAN Elemental
+InfoStats["SHAMAN-262"] = "Fire: Intellect > Mastery > Haste > Versatility > Critical Strike \n Lightning: Intellect > Haste > Critical Strike > Versatility > Mastery"
+--SHAMAN Enhancement
+InfoStats["SHAMAN-263"] = "Elem: Agility > Mastery > Haste > Critical Strike > Versatility > Critical Strike \n Storm: Agility > Haste > Mastery > Critical Strike > Versatility"
+--SHAMAN Restoration
+InfoStats["SHAMAN-264"] = "Intellect > Versatility = Critical Strike > Haste = Mastery"
 
-IcyVeinStats["WARRIOR-71"] = "Str > Crit > Haste > Mast > Vers" --// Arms Warrior
-IcyVeinStats["WARRIOR-72"] = "Str > Haste > Mast > Crit > Vers" --// Fury WARRIOR
-IcyVeinStats["WARRIOR-73"] = "Str > Haste > Vers ≥ Crit > Mast" --// Protection Warrior
+--WARLOCK Affliction
+InfoStats["WARLOCK-265"] = "Intellect > Haste 30% > Mastery > Versatility = Critical Strike"
+--WARLOCK Demonology
+InfoStats["WARLOCK-266"] = "Intellect > Haste 24% > Critical Strike = Versatility > Mastery"
+--WARLOCK Destruction
+InfoStats["WARLOCK-267"] = "Intellect > Haste 30% > Critical Strike = Versatility > Mastery"
 
-IcyVeinStats["DEMONHUNTER-577"] = "|cFFFFFF00ST:|r Crit > Haste > Mast > Vers |cFFFFFF00AoE:|r Crit > Mast > Vers > Haste" --// Havoc Demon Hunter
-IcyVeinStats["DEMONHUNTER-581"] = "Agil > Haste > Vers = Crit > Mast" --// Vengeance Demon Hunter
+--WARR Arms
+L["WARRIORArms"] = "Strength > Haste > Critical Strike > Versatility > Mastery"
+--WARR Fury
+L["WARRIORFury"] = "Strength > Haste > Mastery > Versatility > Critical Strike"
+--WARR Protection
+L["WARRIORProtection"] = "Strength > Haste > Versatility = Critical Strike > Mastery"
 
-ViksUI_StatTable = IcyVeinStats
+ViksUI_StatTable = InfoStats
 
 ViksUI_STATS = CreateFrame("Frame",ViksUI_STATS,UIParent)
 
 function ViksUI_STATS:CreateWin()
     if PaperDollFrame:IsVisible() then
         if not ViksUI_STATSwin then
-            ViksUI_STATSwin = CreateFrame("Frame",ViksUI_STATSwin,ViksUI_STATS)
+			ViksUI_STATSwin = CreateFrame("Frame",ViksUI_STATSwin,ViksUI_STATS)
 			local f = ViksUI_STATS
+			f:SetParent(CharacterFrame)
 			f:SetTemplate("Overlay")
-            f:SetFrameStrata("TOOLTIP")
-   
-            f:SetWidth(CharacterFrame:GetWidth()) 
-            f:SetHeight(25)
-    	    ViksUI_STATStxt = f:CreateFontString(nil,"OVERLAY","GameFontWhite")
+			f:SetFrameStrata("TOOLTIP")
+			f:ClearAllPoints()
+			f:SetPoint("BOTTOMRIGHT",CharacterFrame,"TOPRIGHT", 0,3)
+			f:SetPoint("TOPLEFT",CharacterFrame,"TOPLEFT", 0,30)
+
+			ViksUI_STATStxt = f:CreateFontString(nil,"OVERLAY")
+			ViksUI_STATStxt:SetFont(C.media.pxfontHeader, C.media.pxfontHsize)
 			local ft = ViksUI_STATStxt 
 			ft:ClearAllPoints()
 			ft:SetAllPoints(ViksUI_STATS) 
 			ft:SetJustifyH("CENTER")
-			ft:SetJustifyV("MIDDLE")
-            f:ClearAllPoints()
-            f:SetPoint("BOTTOMRIGHT",PaperDollFrame,"TOPRIGHT", 0,3)
-            f:SetParent(PaperDollFrame)
-            f:Show()            
+			ft:SetJustifyV("MIDDLE")            
+			f:Show()            
         end
         return true
     end
@@ -89,17 +130,32 @@ function ViksUI_STATS:Update()
     if ViksUI_STATS:CreateWin() then
         local _, className = UnitClass("player")
 		local class = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
-		local classcolor = T.RGBToHex(class.r, class.g, class.b)
+		local classcolor = T.RGBToHex(unpack(C.media.border_color))
+		local statcolor = T.RGBToHex(class.r, class.g, class.b)
         local sId, specName = GetSpecializationInfo(GetSpecialization())
         local s = ViksUI_StatTable[className .. "-" .. sId]
         if s then
-            s = gsub(s,"Str","STR")
-            s = gsub(s,"Agil","AGI")
-            s = gsub(s,"Intelligence","INT")
-            s = gsub(s,"Stamina","STA")
-            ViksUI_STATStxt:SetText(classcolor..specName..": "..s) 
-        end               
-    end
+            s = gsub(s,"Agility","AGI")
+            s = gsub(s,"Intellect","INT")
+            s = gsub(s,"Critical Strike","CRIT")
+            s = gsub(s,"Single Target:","|cFFFFFF00ST:|r")
+            s = gsub(s,"Multiple Target:","|cFFFFFF00AoE:|r")
+            s = gsub(s,"Item Level","iLvL")
+            s = gsub(s,"Haste","HAST")
+            s = gsub(s,"Versatility","VERS")
+            s = gsub(s,"Mastery","MAST")
+            s = gsub(s,"Strength","STR")
+            s = gsub(s,"Myth+:","|cFFFFFF00M+:|r")
+            s = gsub(s,"Raid:","|cFFFFFF00Raid:|r")
+            s = gsub(s,"Weapon Damage","WDMG")
+            ViksUI_STATStxt:SetText(classcolor..specName..": "..statcolor..s) 
+        end
+		if strlen(s) > 70 then
+			ViksUI_STATS:SetPoint("TOPLEFT",CharacterFrame,"TOPLEFT", 0,48)
+		else
+			ViksUI_STATS:SetPoint("TOPLEFT",CharacterFrame,"TOPLEFT", 0,30)
+		end
+	end
 end
 
 local f = ViksUI_STATS
