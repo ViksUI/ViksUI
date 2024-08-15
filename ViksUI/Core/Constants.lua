3e9094360d4f039d4533074d95a051df081e16f3
Frode <frode.h@aktivportal.com>
2024-08-15 18:19:39 +0200

    Cleanup

--- ViksUI/Core/Constants.lua
+++ ViksUI/Core/Constants.lua
@@ -16,7 +16,7 @@ T.screenWidth, T.screenHeight = GetPhysicalScreenSize()
 T.newPatch = select(4, GetBuildInfo()) >= 110000
 T.Retail =_G.WOW_PROJECT_ID == _G.WOW_PROJECT_MAINLINE
 
--- BETA
+-- NOTE: Restore old function
 GetContainerItemInfo = function(bagIndex, slotIndex)
 	local info = C_Container.GetContainerItemInfo(bagIndex, slotIndex)
 	if info then
@@ -39,20 +39,20 @@ GetSpellInfo = function(data)
 	end
 end
 
-local function EasyMenu_Initialize( frame, level, menuList )
+local function EasyMenu_Initialize(frame, level, menuList)
 	for index = 1, #menuList do
 		local value = menuList[index]
-		if (value.text) then
-			value.index = index;
-			UIDropDownMenu_AddButton( value, level );
+		if value.text then
+			value.index = index
+			UIDropDownMenu_AddButton(value, level)
 		end
 	end
 end
 
-function EasyMenu(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay )
-	if ( displayMode == "MENU" ) then
-		menuFrame.displayMode = displayMode;
+function EasyMenu(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay)
+	if displayMode == "MENU"  then
+		menuFrame.displayMode = displayMode
 	end
-	UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList);
-	ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay);
+	UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList)
+	ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay)
 end
