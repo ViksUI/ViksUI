cf668444ed240526e2c8d8adefd46282ddd30736
Frode <frode.h@aktivportal.com>
2024-08-29 08:50:05 +0200

    [Skins] Added skin for PlayerSpellsFrame.TalentsFrame.HeroTalentsContainer.PreviewContainer.

--- ViksUI/Modules/Skins/Blizzard/Talent.lua
+++ ViksUI/Modules/Skins/Blizzard/Talent.lua
@@ -72,6 +72,41 @@ local function LoadSkin()
 		end
 	end
 
+	local heroTalentPreview = PlayerSpellsFrame.TalentsFrame.HeroTalentsContainer.PreviewContainer
+	heroTalentPreview.Background:SetAlpha(0)
+	heroTalentPreview:CreateBackdrop("Transparent")
+	heroTalentPreview.backdrop:SetInside(heroTalentPreview, 20, 40)
+
+	local TalentsSelect = _G.HeroTalentsSelectionDialog
+	if TalentsSelect then
+		T.SkinFrame(TalentsSelect)
+
+		hooksecurefunc(TalentsSelect, "ShowDialog", function(frame)
+			if not frame then return end
+
+			for specFrame in frame.SpecContentFramePool:EnumerateActive() do
+				if specFrame and not specFrame.IsSkinned then
+					--FIXME Need to test if specFrame.SpecName then specFrame.SpecName:SetFont(C.media.normal_font, 18) end
+					-- if specFrame.Description then specFrame.Description:SetFont(C.media.normal_font, 14) end
+					-- if specFrame.CurrencyFrame then
+						-- specFrame.CurrencyFrame.LabelText:SetFont(C.media.normal_font, 13)
+						-- specFrame.CurrencyFrame.AmountText:SetFont(C.media.normal_font, 18)
+					-- end
+					specFrame.ActivateButton:SkinButton()
+					specFrame.ApplyChangesButton:SkinButton()
+
+					specFrame.IsSkinned = true
+				end
+			end
+		end)
+	end
+
+	-- PvP
+	PlayerSpellsFrame.TalentsFrame.PvPTalentList:StripTextures()
+	PlayerSpellsFrame.TalentsFrame.PvPTalentList:CreateBackdrop("Overlay")
+	PlayerSpellsFrame.TalentsFrame.PvPTalentList.backdrop:SetFrameStrata(PlayerSpellsFrame.TalentsFrame.PvPTalentList:GetFrameStrata())
+	PlayerSpellsFrame.TalentsFrame.PvPTalentList.backdrop:SetFrameLevel(2000)
+
 	-- Spec tab
 	PlayerSpellsFrame.SpecFrame:CreateBackdrop("Overlay")
 	PlayerSpellsFrame.SpecFrame.backdrop:SetPoint("TOPLEFT", 4, -4)
@@ -105,12 +140,6 @@ local function LoadSkin()
 			end
 		end
 	end)
-
-	-- PvP
-	PlayerSpellsFrame.TalentsFrame.PvPTalentList:StripTextures()
-	PlayerSpellsFrame.TalentsFrame.PvPTalentList:CreateBackdrop("Overlay")
-	PlayerSpellsFrame.TalentsFrame.PvPTalentList.backdrop:SetFrameStrata(PlayerSpellsFrame.TalentsFrame.PvPTalentList:GetFrameStrata())
-	PlayerSpellsFrame.TalentsFrame.PvPTalentList.backdrop:SetFrameLevel(2000)
 
 	-- SpellBook
 	local page = PlayerSpellsFrame.SpellBookFrame
