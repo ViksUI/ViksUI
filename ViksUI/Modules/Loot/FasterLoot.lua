ec6dced3cf56f3964be64d605d44f2e698b24d0e
Frode <frode.h@aktivportal.com>
2024-09-29 17:46:32 +0200

    Cleanup

--- ViksUI/Modules/Loot/FasterLoot.lua
+++ ViksUI/Modules/Loot/FasterLoot.lua
@@ -7,9 +7,6 @@ if C.loot.faster_loot ~= true then return end
 local tDelay = 0
 local LOOT_DELAY = 0.3
 
-LOOTFRAME_AUTOLOOT_DELAY = 0.1;
-LOOTFRAME_AUTOLOOT_RATE = 0.1;
-	
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("LOOT_READY")
 frame:SetScript("OnEvent", function ()
