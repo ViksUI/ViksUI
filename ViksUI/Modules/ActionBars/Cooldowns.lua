a71b898c9cca6fddb218149911b5effbcb4b6f6b
Frode <frode.h@aktivportal.com>
2025-04-25 18:51:22 +0700

    [Cooldowns] Change logic for check setHideCooldownNumbers.

--- ViksUI/Modules/ActionBars/Cooldowns.lua
+++ ViksUI/Modules/ActionBars/Cooldowns.lua
@@ -91,6 +91,7 @@ local function Timer_Create(self)
 	Timer_OnSizeChanged(timer, scaler:GetSize())
 	scaler:SetScript("OnSizeChanged", function(_, ...) Timer_OnSizeChanged(timer, ...) end)
 
+	self:SetHideCountdownNumbers(true)
 	self:GetRegions():SetAlpha(0)	-- Hide Blizzard cd text
 
 	self.timer = timer
@@ -108,7 +109,9 @@ local function deactivateDisplay(cooldown)
 end
 
 local function setHideCooldownNumbers(cooldown, hide)
-	if hide then
+	local disable = not (hide or cooldown.noCooldownCount or cooldown:IsForbidden())
+
+	if disable then
 		hideNumbers[cooldown] = true
 		deactivateDisplay(cooldown)
 	else
@@ -141,5 +144,5 @@ hooksecurefunc(Cooldown_MT, "Clear", deactivateDisplay)
 hooksecurefunc(Cooldown_MT, "SetHideCountdownNumbers", setHideCooldownNumbers)
 
 hooksecurefunc("CooldownFrame_SetDisplayAsPercentage", function(cooldown)
-	setHideCooldownNumbers(cooldown, true)
+	setHideCooldownNumbers(cooldown, false)
 end)
