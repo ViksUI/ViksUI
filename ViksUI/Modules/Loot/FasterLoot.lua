2bc225610558427132e0194a96df070b78dfc510
Frode <frode.h@aktivportal.com>
2026-03-11 16:27:21 +0700

    [Loot] Added workaround for auto loot to prevent stop looting.

--- ViksUI/Modules/Loot/FasterLoot.lua
+++ ViksUI/Modules/Loot/FasterLoot.lua
@@ -1,72 +1,39 @@
 local T, C, L = unpack(ViksUI)
-if C.loot.faster_loot ~= true then return end
 
 ----------------------------------------------------------------------------------------
 --	Faster auto looting
 ----------------------------------------------------------------------------------------
-local addonName, addonTable = ...
+if C.loot.faster_loot then
+	local tDelay = 0
+	local LOOT_DELAY = 0.3
 
-local majorVersion = select(4, GetBuildInfo())
-
-local tDelay = 0
-local LOOT_DELAY = 0.3
-
-local IsBagFull
-
-if majorVersion >= 10 then
-    IsBagFull = function()
-        for bag = 0, 4 do
-            local numSlots = C_Container.GetContainerNumSlots(bag)
-            for slot = 1, numSlots do
-                if not C_Container.GetContainerItemInfo(bag, slot) then
-                    return false
-                end
-            end
-        end
-        return true
-    end
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("LOOT_READY")
+	frame:SetScript("OnEvent", function()
+		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+			if (GetTime() - tDelay) >= LOOT_DELAY then
+				for i = GetNumLootItems(), 1, -1 do
+					LootSlot(i)
+				end
+				tDelay = GetTime()
+			end
+		end
+	end)
 else
-    IsBagFull = function()
-        for bag = 0, 4 do
-            for slot = 1, GetContainerNumSlots(bag) do
-                if not GetContainerItemID(bag, slot) then
-                    return false
-                end
-            end
-        end
-        return true
-    end
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("LOOT_OPENED")
+	frame:SetScript("OnEvent", function()
+		local numItems = GetNumLootItems()
+
+		if numItems == 0 then
+		   CloseLoot()
+		   return
+		end
+
+		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+			for i = numItems, 1, -1 do
+				LootSlot(i)
+			end
+		end
+	end)
 end
-
-local EventFrame = CreateFrame('Frame')
-
-local function OnEvent(self, event, ...)
-    if event == 'ADDON_LOADED' then
-        local name = ...
-        if name == addonName then
-            if not GetCVarBool("autoLootDefault") then
-                SetCVar("autoLootDefault", "1")
-                print("Auto loot has been enabled.")
-            end
-            self:UnregisterEvent('ADDON_LOADED')
-        end
-    elseif event == 'LOOT_OPENED' or event == 'LOOT_READY' then
-        if GetCVarBool("autoLootDefault") and (GetTime() - tDelay) >= LOOT_DELAY then
-            for i = GetNumLootItems(), 1, -1 do
-                if LootSlotHasItem(i) then
-                    LootSlot(i)
-                end
-            end
-            tDelay = GetTime()
-
-            if event == 'LOOT_OPENED' and not IsBagFull() then
-                LootFrame:Hide()
-            end
-        end
-    end
-end
-
-EventFrame:RegisterEvent('ADDON_LOADED')
-EventFrame:RegisterEvent('LOOT_OPENED')
-EventFrame:RegisterEvent('LOOT_READY')
-EventFrame:SetScript('OnEvent', OnEvent)
