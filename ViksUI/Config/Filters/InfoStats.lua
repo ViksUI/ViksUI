8d28c5f6b1649f0ddfe838fae9c117b925adb61a
Frode <frode.h@aktivportal.com>
2026-04-25 22:52:26 +0700

    Stat Priority updated

--- ViksUI/Config/Filters/InfoStats.lua
+++ ViksUI/Config/Filters/InfoStats.lua
@@ -5,45 +5,127 @@ local T, C, L = unpack(ViksUI)
 
 InfoStats = {}
 
+-- DEATHKNIGHT Blood (250)
 InfoStats["DEATHKNIGHT-250"] = [[Deathbringer: Item Level > Critical Strike / Versatility / Mastery > Haste
 San'layn: Item Level > Haste > Critical Strike / Versatility / Mastery]]
+
+-- DEATHKNIGHT Frost (251)
 InfoStats["DEATHKNIGHT-251"] = [[General: Mastery > Critical Strike > Haste > Versatility]]
+
+-- DEATHKNIGHT Unholy (252)
 InfoStats["DEATHKNIGHT-252"] = [[General: Mastery > Critical Strike > Haste > Versatility]]
+
+-- DEMONHUNTER Havoc (577)
 InfoStats["DEMONHUNTER-577"] = [[General: Agility > Critical Strike > Mastery > Haste > Versatility]]
+
+-- DEMONHUNTER Vengeance (581)
 InfoStats["DEMONHUNTER-581"] = [[General: Agility > Haste > Versatility / Critical Strike > Mastery]]
+
+-- DEMONHUNTER Devourer (1480)
+InfoStats["DEMONHUNTER-1480"] = [[General: Intellect > Haste > Mastery > Critical Strike > Versatility]]
+
+-- DRUID Balance (102)
 InfoStats["DRUID-102"] = [[General: Intellect > Haste > Critical Strike > Mastery > Versatility]]
-InfoStats["DRUID-103"] = [[Druid of the Claw: Agility > Mastery > Versatility
+
+-- DRUID Feral (103)
+InfoStats["DRUID-103"] = [[Druid of the Claw: Agility > Mastery > Haste / Critical Strike > Versatility
 Wildstalker: Agility > Mastery > Haste > Critical Strike > Versatility]]
+
+-- DRUID Guardian (104)
 InfoStats["DRUID-104"] = [[General: Item Level > Agility / Stamina > Haste > Versatility > Mastery > Critical Strike]]
+
+-- DRUID Restoration (105)
 InfoStats["DRUID-105"] = [[General: Intellect > Haste > Versatility > Critical Strike > Mastery]]
+
+-- EVOKER Devastation (1467)
 InfoStats["EVOKER-1467"] = [[General: Item Level > Haste > Critical Strike > Mastery > Versatility]]
+
+-- EVOKER Preservation (1468)
 InfoStats["EVOKER-1468"] = [[General: Intellect > Mastery > Haste > Critical Strike > Versatility]]
-InfoStats["EVOKER-1473"] = [[General: Item Level > Critical Strike > Haste > Mastery]]
+
+-- EVOKER Augmentation (1473)
+InfoStats["EVOKER-1467"] = [[General: Item Level > Critical Strike > Haste > Mastery (MoP Builds) > Mastery = Versatility]]
+
+-- HUNTER Beast Mastery (253)
 InfoStats["HUNTER-253"] = [[General: Mastery > Critical Strike > Haste > Versatility]]
-InfoStats["HUNTER-254"] = [[General: Versatility > Haste]]
+
+-- HUNTER Marksmanship (254)
+InfoStats["HUNTER-254"] = [[General: Critical Strike > Mastery > Versatility > Haste]]
+
+-- HUNTER Survival (255)
 InfoStats["HUNTER-255"] = [[General: Mastery > Critical Strike > Haste > Versatility]]
+
+-- MAGE Arcane (62)
 InfoStats["MAGE-62"] = [[General: Intellect > Mastery > Versatility > Critical Strike > Haste]]
+
+-- MAGE Fire (63)
 InfoStats["MAGE-63"] = [[General: Intellect > Haste > Mastery > Versatility > Critical Strike]]
+
+-- MAGE Frost (64)
 InfoStats["MAGE-64"] = [[General: Intellect > Mastery > Critical Strike > Haste > Versatility]]
+
+-- MONK Brewmaster (268)
+InfoStats["MONK-268"] = [[General: Critical Strike / Versatility / Mastery > Versatility]]
+
+-- MONK Windwalker (269)
 InfoStats["MONK-269"] = [[General: Agility > Haste > Critical Strike > Mastery > Versatility]]
+
+-- MONK Mistweaver (270)
 InfoStats["MONK-270"] = [[General: Intellect > Haste > Critical Strike > Versatility > Mastery]]
+
+-- PALADIN Holy (65)
 InfoStats["PALADIN-65"] = [[General: Intellect > Mastery > Haste > Critical Strike > Versatility]]
-InfoStats["PALADIN-66"] = [[General: Haste > Critical Strike > Versatility / Mastery]]
+
+-- PALADIN Protection (66)
+InfoStats["PALADIN-66"] = [[General: Haste > Versatility / Mastery > Critical Strike]]
+
+-- PALADIN Retribution (70)
 InfoStats["PALADIN-70"] = [[General: Strength > Mastery > Critical Strike > Haste > Versatility]]
-InfoStats["PRIEST-256"] = [[General: Haste > Critical Strike > Mastery > Versatility]]
-InfoStats["PRIEST-257"] = [[General: Critical Strike > Haste]]
+
+-- PRIEST Discipline (256)
+InfoStats["PRIEST-256"] = [[General: Haste > Critical Strike > Versatility > Mastery]]
+
+-- PRIEST Holy (257)
+InfoStats["PRIEST-257"] = [[General: Critical Strike > Versatility / Mastery > Haste]]
+
+-- PRIEST Shadow (258)
 InfoStats["PRIEST-258"] = [[General: Intellect > Haste > Mastery > Critical Strike > Versatility]]
+
+-- ROGUE Assassination (259)
 InfoStats["ROGUE-259"] = [[General: Critical Strike / Haste > Mastery > Versatility]]
+
+-- ROGUE Outlaw (260)
 InfoStats["ROGUE-260"] = [[General: Critical Strike > Haste > Versatility > Mastery]]
-InfoStats["ROGUE-261"] = [[General: Haste > Mastery > Critical Strike > Versatility]]
-InfoStats["SHAMAN-263"] = [[Stormbringer: Agility > Haste > Versatility
+
+-- ROGUE Subtlety (261)
+InfoStats["ROGUE-261"] = [[General: Haste > Critical Strike > Mastery > Versatility]]
+
+-- SHAMAN Elemental (262)
+InfoStats["SHAMAN-262"] = [[General: Intellect > Mastery 50% > Haste 25% > Critical Strike 25%]]
+
+-- SHAMAN Enhancement (263)
+InfoStats["SHAMAN-263"] = [[Stormbringer: Agility > Haste > Mastery / Critical Strike > Versatility
 Totemic: Agility > Mastery > Haste > Critical Strike > Versatility]]
-InfoStats["SHAMAN-264"] = [[General: Item Level > Critical Strike > Haste]]
+
+-- SHAMAN Restoration (264)
+InfoStats["SHAMAN-264"] = [[General: Item Level > Critical Strike > Versatility / Mastery > Haste]]
+
+-- WARLOCK Affliction (265)
 InfoStats["WARLOCK-265"] = [[General: Intellect > Mastery > Versatility]]
+
+-- WARLOCK Demonology (266)
 InfoStats["WARLOCK-266"] = [[General: Intellect > Mastery > Versatility]]
+
+-- WARLOCK Destruction (267)
 InfoStats["WARLOCK-267"] = [[General: Intellect > Critical Strike > Haste > Mastery > Versatility]]
+
+-- WARRIOR Arms (71)
 InfoStats["WARRIOR-71"] = [[General: Strength > Critical Strike > Haste > Mastery > Versatility]]
+
+-- WARRIOR Fury (72)
 InfoStats["WARRIOR-72"] = [[General: Strength > Mastery > Haste > Versatility > Critical Strike]]
-InfoStats["WARRIOR-73"] = [[General: Item Level > Haste > Mastery]]
+
+-- WARRIOR Protection (73)
+InfoStats["WARRIOR-73"] = [[General: Haste > Versatility / Critical Strike]]
 
 return InfoStats
