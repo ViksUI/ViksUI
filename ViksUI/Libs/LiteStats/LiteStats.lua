3199c0812bcb19813628c1f8fb143996d0e12c6a
ViksUI <frode.h@aktivportal.com>
2026-07-20 21:46:50 +0200

    Update LiteStats.lua

--- ViksUI/Libs/LiteStats/LiteStats.lua
+++ ViksUI/Libs/LiteStats/LiteStats.lua
@@ -1702,9 +1702,6 @@ if ping.enabled then
 
 	Inject("Ping", {
 		OnLoad = function(self)
-			if not T.newPatch then -- 12.1.0 Can use only Blizzard
-				self:RegisterEventCallback("MINIMAP_PING", OnEvent)
-			end
 			self.animGroup = self.text:CreateAnimationGroup()
 			self.anim = self.animGroup:CreateAnimation("Alpha")
 			self.animGroup:SetScript("OnFinished", function() self.text:Hide() end)
