5adeb86c3e7e43283eb0c6396eb62e5079f53053
ViksUI <frode.h@aktivportal.com>
2026-07-20 21:47:51 +0200

    Update CooldownViewer.lua

--- ViksUI/Modules/Skins/Blizzard/CooldownViewer.lua
+++ ViksUI/Modules/Skins/Blizzard/CooldownViewer.lua
@@ -3,7 +3,7 @@ local T, C, L = unpack(ViksUI)
 if C.skins.blizzard_frames ~= true then return end
 
 -- Skip if EnhancedCooldownManager or BetterCooldownManager is loaded
-if C_AddOns.IsAddOnLoaded("EnhancedCooldownManager") or C_AddOns.IsAddOnLoaded("BetterCooldownManager") or C_AddOns.IsAddOnLoaded("Ayije_CDM") then return end
+if C_AddOns.IsAddOnLoaded("EnhancedCooldownManager") or C_AddOns.IsAddOnLoaded("BetterCooldownManager") or C_AddOns.IsAddOnLoaded("Ayije_CDM") or C_AddOns.IsAddOnLoaded("EllesmereUI") then return end
 
 
 ----------------------------------------------------------------------------------------
